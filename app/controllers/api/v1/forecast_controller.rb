12d51f56560b5f4f06111122f9afab6896314da0
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-22 19:45:02 -0600

    feature: add route for get /forecast endpoint. Define private method fetch_coordinates in forecast_controller. Create WeatherForecastSerializer and class method weather that formats data from WeatherForecastFacade to render json when endpoint is hit. Tested in Postman, looks good.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::ForecastController < ApplicationController 
-  
+  before_action :fetch_coordinates
+
   def index 
-    # forecast = ForecastFacade.forecast()
+    forecast = WeatherForecastFacade.forecast(@coordinates[:lat], @coordinates[:lng])
+    render json: WeatherForecastSerializer.weather(forecast)
   end 
+
+private 
+
+  def fetch_coordinates
+    if params[:location]
+      @coordinates = LocationFacade.coordinates(params[:location])
+    end 
+  end
 end
---
+++ app/serializers/weather_forecast_serializer.rb
@@ -0,0 +1,45 @@
+class WeatherForecastSerializer
+  def self.weather(forecast)
+    { "data":
+      {
+        "id": nil,
+        "type": 'forecast',
+        "attributes":
+         {
+            "current_weather":
+            {
+              "datetime": Time.at(forecast[:current][:dt]),
+              "temp": forecast[:current][:temp],
+              "feels_like": forecast[:current][:feels_like],
+              "humidity": forecast[:current][:humidity],
+              "sunrise": Time.at(forecast[:current][:sunrise]),
+              "sunset": Time.at(forecast[:current][:sunset]),
+              "uvi": forecast[:current][:uvi],
+              "visibility": forecast[:current][:visibility],
+              "conditions": forecast[:current][:weather][0][:description],
+              "icon": forecast[:current][:weather][0][:icon]
+            },
+            "daily_weather": forecast[:daily].map do |day|
+                {
+                  "datetime": Date.jd(day[:dt]),
+                  "sunrise": Time.at(day[:sunrise]),
+                  "sunset": Time.at(day[:sunset]),
+                  "max_temp": day[:temp][:max],
+                  "min_temp": day[:temp][:min],
+                  "conditions": day[:weather][0][:description],
+                  "icon": day[:weather][0][:icon]
+                }
+            end,
+            "hourly_weather": forecast[:hourly].map do |hour|
+              {
+              "time": Time.at(hour[:dt]),
+              "temp": hour[:temp],
+              "conditions": hour[:weather][0][:description],
+              "icon": hour[:weather][0][:icon]
+              }
+            end
+          },
+      },
+    }
+  end
+end
