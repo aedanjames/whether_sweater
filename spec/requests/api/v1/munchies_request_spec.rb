7fe58d85a894ed4d480c4a778f90328c6a47609c
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-25 10:40:57 -0600

    test: add restaurant expects to munchies_request_spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'get /forecast' do
+RSpec.describe 'get /munchies' do
 
   it 'returns json data to plan for munchies', :vcr do 
     headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
@@ -20,11 +20,15 @@ RSpec.describe 'get /forecast' do
     expect(munchie_response[:data][:id]).to eq("null")
     expect(munchie_response[:data][:type]).to eq("munchie")
     expect(munchie_response[:data][:attributes]).to be_a Hash
-    expect(munchie_response[:data][:attributes][:destination_city]).to eq("Pueblo, co")
-    expect(munchie_response[:data][:attributes][:travel_time]).to eq("01:45:28")
+    expect(munchie_response[:data][:attributes][:destination_city]).to be_an String
+    expect(munchie_response[:data][:attributes][:travel_time]).to be_an String
     expect(munchie_response[:data][:attributes][:forecast]).to be_a Hash
-    expect(munchie_response[:data][:attributes][:forecast][:summary]).to eq("few clouds")
-    expect(munchie_response[:data][:attributes][:forecast][:temperature]).to eq("45.32")
+    expect(munchie_response[:data][:attributes][:forecast][:summary]).to be_an String
+    expect(munchie_response[:data][:attributes][:forecast][:temperature]).to be_an String
+    expect(munchie_response[:data][:attributes][:restaurant]).to be_an Hash
+    expect(munchie_response[:data][:attributes][:restaurant][:name]).to be_an String
+    expect(munchie_response[:data][:attributes][:restaurant][:name]).to eq("Kan's Kitchen")
+    expect(munchie_response[:data][:attributes][:restaurant][:address]).to be_an String
     
   end
 end
