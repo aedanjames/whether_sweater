812a4e70c222c970dfa0790e9fe2505e70b46b0d
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-25 09:57:45 -0600

    test: write test for location_service class methods route_connection, and get_route. Tests pass

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -12,4 +12,19 @@ class LocationService
     end
     JSON.parse(response.body, symbolize_names: true)
   end 
+
+  def self.route_connection
+    url = "http://www.mapquestapi.com/directions/v2/route"
+    Faraday.new(url: url) do |faraday|
+      faraday.params['key'] = ENV['mapquest_api_key']
+    end 
+  end
+
+  def self.get_route(from, to)
+    response = route_connection.get do |faraday|
+      faraday.params['from'] = from
+      faraday.params['to'] = to
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe 'Location Service' do
       expect(connection).to be_a(Faraday::Connection)
   end
 
+  it 'returns a connection for get_route connection', :vcr do
+      connection = LocationService.route_connection
+      expect(connection).to be_a(Faraday::Connection)
+  end
+
   it 'returns longitude and latitude coorindates', :vcr do
     coordinates = LocationService.get_coordinates("Sheridan,CO")
 
@@ -16,5 +21,12 @@ RSpec.describe 'Location Service' do
     expect(coordinates[:results][0][:locations][0]).to be_a Hash
     expect(coordinates[:results][0][:locations][0][:latLng]).to have_key(:lat)
     expect(coordinates[:results][0][:locations][0][:latLng]).to have_key(:lng)
+  end
+
+  it 'returns the route given from and to params', :vcr do
+    route = LocationService.get_route("39.64821 -104.98796", "39.64693 -105.02506")
+    expect(route[:route]).to be_a Hash
+    expect(route[:route][:formattedTime]).to be_a String
+
   end
 end
