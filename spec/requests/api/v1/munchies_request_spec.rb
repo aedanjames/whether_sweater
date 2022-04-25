16c1fde080849b63d0a805bb5973d8798f215452
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-25 10:28:55 -0600

    test: write munchies GET '/munchies' request spec

---
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'get /forecast' do
+
+  it 'returns json data to plan for munchies', :vcr do 
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    params = {
+      "start": "Denver, co",
+      "destination":  "Pueblo, co",
+      "food": "Chinese"
+    }
+
+    get '/api/v1/munchies', headers: headers, params: params
+
+    munchie_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(munchie_response[:data]).to be_a Hash
+    expect(munchie_response[:data][:id]).to eq("null")
+    expect(munchie_response[:data][:type]).to eq("munchie")
+    expect(munchie_response[:data][:attributes]).to be_a Hash
+    expect(munchie_response[:data][:attributes][:destination_city]).to eq("Pueblo, co")
+    expect(munchie_response[:data][:attributes][:travel_time]).to eq("01:45:28")
+    expect(munchie_response[:data][:attributes][:forecast]).to be_a Hash
+    expect(munchie_response[:data][:attributes][:forecast][:summary]).to eq("few clouds")
+    expect(munchie_response[:data][:attributes][:forecast][:temperature]).to eq("45.32")
+    
+  end
+end
