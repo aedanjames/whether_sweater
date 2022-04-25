f7cd5dc99fd91fa1566f3909abb531e303470e2c
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-25 11:54:02 -0600

    feature: arrival time is taken into account for restaurants that are displayed. Only restaurants that are anticipated to be open at the arrival time are returned

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     travel_time = LocationService.get_route(@start_coordinates.values.join(','), @destination_coordinates.values.join(','))[:route][:formattedTime]
     arrival_time = MunchiesSerializer.arrival_time(travel_time)
-    restaurant = YelpService.get_business(@destination_coordinates[:lat], @destination_coordinates[:lng], params[:food])
+    restaurant = YelpService.get_business(@destination_coordinates[:lat], @destination_coordinates[:lng], params[:food], arrival_time)
     forecast = WeatherForecastFacade.forecast(@destination_coordinates[:lat], @destination_coordinates[:lng])
     render json: MunchiesSerializer.potential_munch(params[:destination], travel_time, restaurant, forecast)
   end 
